de49939de238fb87912c15d762062dbb40babd70
tdeepika16 <35536160+tdeepika16@users.noreply.github.com>
2018-01-18 22:36:35 +0530

    Initial project setup

---
+++ marsrover/src/main/java/com/rover/marsrover/App.java
@@ -0,0 +1,17 @@
+package com.rover.marsrover;
+
+import com.rover.utils.InputUtils;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	System.out.println("Main");
+    }
+}
